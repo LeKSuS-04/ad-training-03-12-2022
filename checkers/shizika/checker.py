610e8397f19f8c2917196b72d5d6a039a11a1881
Alexey Tarasov <tarasovion2004@gmail.com>
2022-12-02 21:37:37 +0300

    :))))))))))

--- checkers/shizika/checker.py
+++ checkers/shizika/checker.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import re
+import os
 import sys
 from typing import Union
 argv = [c for c in sys.argv]        # https://docs.pwntools.com/en/stable/args.html :)))))))))))
+os.environ['PWNLIB_NOTERM'] = '1'   # https://stackoverflow.com/a/67183309/15078906 :)))))))))))
 
 from pwn import remote, PwnlibException, context
 from checklib import *
